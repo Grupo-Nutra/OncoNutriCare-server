330763e88ac266d3f3d65d424cbbdd2930c4b1ee
joaobisi <jbisinotti@gmail.com>
2023-08-31 20:10:18 -0300

    feat(#20): criação da model de nutricionista

---
+++ server/src/models/nutritionist.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+const db = require('../util/db');
+
+const Nutricionista = db.define('Nutricionista', {
+    idNutricionista: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    crnSigla: {
+        type: Sequelize.ENUM('CRN-1', 'CRN-2', 'CRN-3', 'CRN-4', 'CRN-5', 'CRN-6', 'CRN-7', 'CRN-8', 'CRN-9', 'CRN-10', 'CRN-11'),
+        allowNull: false,
+    },
+    crnNumero: {
+        type: Sequelize.INTEGER(6),
+        allowNull: false,
+    },
+    nomeCompleto: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    telefone: {
+        type: Sequelize.STRING,
+    },
+    email: {
+        type: Sequelize.STRING,
+    }, 
+},  {
+        indexes: [
+            {
+                unique: true,
+                fields: ['crnSigla', 'crnNumero']
+            }
+        ]
+});
+
+module.exports = Nutricionista;

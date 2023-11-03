195d527c9f400038591010a30efb9f24df609e1c
pdaniel37 <pedrodaniel.unb@gmail.com>
2023-11-03 11:55:51 -0300

    feat(#22): refatorando controller consulta

--- server/src/controllers/appointmentsController.js
+++ server/src/controllers/appointmentsController.js
@@ -3,13 +3,25 @@ const log = require('../util/logger');
 
 //create
 exports.createAppointment = async (req, res) => {
-    const { idPaciente, data, peso, diagnosticoNutri, inapetencia, riscoNutricional, statusTno, planoTerapeutico, tratamento } = req.body;
+    const { idPaciente, data, peso, gordura, hemoglobina, leucocitos, plaquetas, nauseas, vomitos, mucosite, diarreia, constipacao, alteracaoPaladar, adesaoTno, aceitacaoAlimentar, diagnosticoNutri, inapetencia, riscoNutricional, statusTno, planoTerapeutico, tratamento } = req.body;
 
     try {
         const appointment = await Consulta.create({
             idPaciente: idPaciente,
             data: data,
             peso: peso,
+            gordura: gordura,
+            hemoglobina: hemoglobina,
+            leucocitos: leucocitos,
+            plaquetas: plaquetas,
+            nauseas: nauseas,
+            vomitos: vomitos,
+            mucosite: mucosite,
+            diarreia: diarreia,
+            constipacao: constipacao,
+            alteracaoPaladar: alteracaoPaladar,
+            adesaoTno: adesaoTno,
+            aceitacaoAlimentar: aceitacaoAlimentar,
             diagnosticoNutri: diagnosticoNutri,
             inapetencia: inapetencia,
             riscoNutricional: riscoNutricional,
@@ -17,8 +29,7 @@ exports.createAppointment = async (req, res) => {
             planoTerapeutico: planoTerapeutico,
             tratamento: tratamento
         });
-        log.info('Consulta criada com sucesso:', appointment);
-        res.status(201).json({ message: 'Consulta criada com sucesso.', appointment });
+        res.status(201).json(appointment);
     } catch (err) {
         log.error('Erro ao criar consulta:', err);
         res.status(500).json({ message: 'Erro ao criar consulta.' });
@@ -27,15 +38,14 @@ exports.createAppointment = async (req, res) => {
 
 //read
 exports.getAppointment = async (req, res) => {
-    const appointmentId = req.params.appointmentId;
+    const idConsulta = req.params.idConsulta;
 
     try {
-        const appointment = await Consulta.findByPk(appointmentId);
+        const appointment = await Consulta.findByPk(idConsulta);
         if (!appointment) {
             return res.status(404).json({ message: 'Consulta não encontrada.' });
         }
-        res.status(200).json({ appointment: appointment });
-        log.info('Consulta encontrada.', appointment.idConsulta);
+        res.status(200).json(appointment);
     } catch (err) {
         log.error(err);
         res.status(500).json({ message: 'Erro ao buscar consulta.' });
@@ -55,8 +65,7 @@ exports.getAppointmentsByPatient = async (req, res) => {
             return res.status(404).json({ message: 'Nenhuma consulta encontrada para este paciente.' });
         }
 
-        log.info('Consultas encontradas para o paciente:', consultas);
-        res.status(200).json({ consultas });
+        res.status(200).json(consultas);
     } catch (err) {
         log.error('Erro ao buscar consultas do paciente:', err);
         res.status(500).json({ message: 'Erro ao buscar consultas do paciente.' });
@@ -65,17 +74,29 @@ exports.getAppointmentsByPatient = async (req, res) => {
 
 //update
 exports.updateAppointment = async (req, res) => {
-    const appointmentId = req.params.appointmentId;
-    const { data, hora, peso, diagnosticoNutri, inapetencia, riscoNutricional, statusTno, planoTerapeutico, tratamento } = req.body;
+    const idConsulta = req.params.idConsulta;
+    const { data, peso, gordura, hemoglobina, leucocitos, plaquetas, nauseas, vomitos, mucosite, diarreia, constipacao, alteracaoPaladar, adesaoTno, aceitacaoAlimentar, diagnosticoNutri, inapetencia, riscoNutricional, statusTno, planoTerapeutico, tratamento } = req.body;
 
     try {
-        const appointment = await Consulta.findByPk(appointmentId);
+        const appointment = await Consulta.findByPk(idConsulta);
         if (!appointment) {
             return res.status(404).json({ message: 'Consulta não encontrada.' });
         }
         appointment.data = data;
         appointment.hora = hora;
         appointment.peso = peso;
+        appointment.gordura = gordura;
+        appointment.hemoglobina = hemoglobina;
+        appointment.leucocitos = leucocitos;
+        appointment.plaquetas = plaquetas;
+        appointment.nauseas = nauseas;
+        appointment.vomitos = vomitos;
+        appointment.mucosite = mucosite;
+        appointment.diarreia = diarreia;
+        appointment.constipacao = constipacao;
+        appointment.alteracaoPaladar = alteracaoPaladar;
+        appointment.adesaoTno = adesaoTno;
+        appointment.aceitacaoAlimentar = aceitacaoAlimentar;
         appointment.diagnosticoNutri = diagnosticoNutri;
         appointment.inapetencia = inapetencia;
         appointment.riscoNutricional = riscoNutricional;
@@ -83,10 +104,8 @@ exports.updateAppointment = async (req, res) => {
         appointment.planoTerapeutico = planoTerapeutico;
         appointment.tratamento = tratamento;
 
-
         const updatedAppointment = await appointment.save();
-        res.status(200).json({ message: 'Consulta atualizada com sucesso.', appointment: updatedAppointment });
-        log.info('Consulta atualizada com sucesso:', updatedAppointment.idConsulta);
+        res.status(200).json(updatedAppointment);
     } catch (err) {
         log.error('Erro ao atualizar consulta:', err);
         res.status(500).json({ message: 'Erro ao atualizar consulta.' });
@@ -104,7 +123,6 @@ exports.deleteAppointment = async (req, res) => {
             return res.status(404).json({ message: 'Consulta não encontrada.' });
         }
         await consulta.destroy();
-        log.info('Consulta removida com sucesso.');
         res.status(200).json({ message: 'Consulta removida com sucesso.' });
     } catch (err) {
         log.error('Erro ao remover consulta:', err);

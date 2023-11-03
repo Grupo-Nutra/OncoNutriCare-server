061756434162a339da0f685960c927fc5f30e61d
pdaniel37 <pedrodaniel.unb@gmail.com>
2023-11-03 00:02:08 -0300

    feat(#22): refatorando model consulta

--- server/src/models/appointment.js
+++ server/src/models/appointment.js
@@ -17,18 +17,65 @@ const Consulta = db.define('Consulta', {
         type: Sequelize.DECIMAL(5,2),
         allowNull: false,
     },
+    gordura: {
+        type: Sequelize.DECIMAL(4,2),
+    },
+    hemoglobina: {
+        type: Sequelize.DECIMAL(3,1),
+    },
+    leucocitos: {
+        type: Sequelize.INTEGER,
+    },
+    plaquetas: {
+        type: Sequelize.INTEGER,
+    },
+    nauseas: {
+        type: Sequelize.ENUM('Não', 'Antecipatória', 'Aguda', 'Tardia'),
+        allowNull: false,
+    },
+    vomitos: {
+        type: Sequelize.ENUM('Não', 'Antecipatória', 'Aguda', 'Tardia'),
+        allowNull: false,
+    },
     diagnosticoNutri: {
         type: Sequelize.ENUM('Baixo peso', 'Desnutrição', 'Caquexia', 'Miopenia', 'Sarcopenia', 'Sobrepeso', 'Obesidade'),
         allowNull: false,
     },
     inapetencia: {
         type: Sequelize.ENUM('Sim', 'Não'),
+        allowNull: false,
+    },
+    mucosite: {
+        type: Sequelize.ENUM('Sim', 'Não'),
+        allowNull: false,
+    },
+    diarreia: {
+        type: Sequelize.ENUM('Sim', 'Não', 'Diarreia Aguda'),
+        allowNull: false,
+    },
+    constipacao: {
+        type: Sequelize.ENUM('Sim', 'Não'),
+        allowNull: false,
+    },
+    alteracaoPaladar: {
+        type: Sequelize.ENUM('Sim', 'Não'),
+        allowNull: false,
     },
     riscoNutricional: {
         type: Sequelize.ENUM('Sim', 'Não'),
+        allowNull: false,
     },
     statusTno: {
         type: Sequelize.ENUM('Sim', 'Não'),
+        allowNull: false,
+    },
+    adesaoTno: {
+        type: Sequelize.ENUM('0%', '25%', '50%', '75%', '100%'),
+    },
+    //ideal? '0 a 20%', '21 a 40%', '41 a 60%', '61 a 80%', '81 a 90%', '91 a 100%'
+    //ideal?? 'Muito Baixa', 'Baixa', 'Moderada', 'Alta', 'Muito Alta'
+    aceitacaoAlimentar: {
+        type: Sequelize.ENUM('0 a 20%', '40%', '60%', '80%', '90 a 100%'),
     },
     planoTerapeutico: {
         type: Sequelize.STRING(350),
